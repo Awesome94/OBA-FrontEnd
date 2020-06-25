35a6a384a57ddabde3a0af58103d80986e5e7504
awesome94 <suubidavid@gmail.com>
2020-06-25 16:49:09 +0300

    Upload component

--- src/components/UploadFile/UploadComponent/UploadFileComponent.js
+++ src/components/UploadFile/UploadComponent/UploadFileComponent.js
@@ -1,7 +1,35 @@
+import React, { useState } from 'react';
+import './UploadFile.css';
+import DropzoneComponent from '../Dropzone/Dropzone';
+
 const UploadFile = (props) => {
-    return ( 
-        <p>Upload</p>
-     );
+    const [Uploading, setUploading] = useState(false)
+    const [UploadProgress, setUploadProgress] = useState({})
+    const [SuccessfullUploaded, setSuccessfullUploaded] = useState(false)
+
+    return (
+        <div className="Upload">
+            <span className="Title">Upload Files</span>
+            <div className="Content">
+                <div>
+                    <Dropzone
+                        onFilesAdded={}
+                        disabled={}
+                    />
+                </div>
+                <div className="Files">
+                    {files.map(file => {
+                        return (
+                            <div key={file.name} className="Row">
+                                <span className="Filename">{file.name}</span>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+            <div className="Actions" />
+        </div>
+    );
 }
- 
+
 export default UploadFile;
