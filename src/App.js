48ed2c2f24638c20eba7716963db4ebc8aaee323
awesome94 <suubidavid@gmail.com>
2020-07-01 17:09:18 +0300

    Add react bootstrap library

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
+
 import './App.css';
 import AuthComponent from './components/Authentication/AuthComponent';
 import RegisterBusinessComponent from './components/RegisterBusiness/RegisterBusinessComponent'
 import UploadFileComponent from './components/UploadFile/UploadComponent/UploadFileComponent'
 import DashboardComponent from './components/Dashboard/Dashboard'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <DashboardComponent/>
-        </div>
+       
+      </div>
     );
   }
 }
---
+++ src/components/Businesses/BusinessList.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Table } from 'react-bootstrap';
+
+
+const BusinessTable = () => {
+    return ( 
+        <Table responsive>
+        <thead>
+          <tr>
+            <th>Business Name</th>
+            <th>Abbreviation</th>
+            <th>Address</th>
+            <th>Country</th>
+            <th>Files Attached</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+          </tr>
+        </tbody>
+      </Table>
+     );
+}
+ 
+export default BusinessTable;
