5f27501e8bea214e16b606bcc94bc0216146218b
awesome94 <suubidavid@gmail.com>
2020-07-08 13:45:24 +0300

    Add user action creators

--- src/_actions/user.actions.js
+++ src/_actions/user.actions.js
@@ -1,1 +1,59 @@
 import { userConstants } from '../_constants';
+import {userService } from '../_services';
+import {alertActions} from './';
+import { history } from "../helpers";
+
+
+export consst userActions = {
+    login,
+    logout,
+    register,
+};
+
+const login = ()=>{
+    return dispatch => {
+        dispatch(request({username}));
+
+        userService.login(username, password)
+            .then(
+                user => {
+                    dispatch(success(user));
+                    history.push('/');
+                },
+                error => {
+                    dispatch(failure(error.toString()));
+                    dispatch(alertActions.error(error.toString()));
+                }
+            );
+    };
+    const request=(user)=>{ return { type: userConstants.LOGIN_REQUEST, user } }
+    const success=(user)=>{ return { type: userConstants.LOGIN_SUCCESS, user } }
+    const failure=(error)=>{ return { type: userConstants.LOGIN_FAILURE, error } }
+}
+
+cosnt logout=()=>{
+    userService.logout();
+    return {type: userConstants.LOGOUT};
+}
+
+const register=user=>{
+    return dispatch=>{
+        dispatch(request(user));
+
+        userService.register(user)
+            .then(
+                user=>{
+                    dispatch(success());
+                    history.push('/login');
+                    dispatch(alertActions.success('Registration Successful'));
+                },
+                error => {
+                    dispatch(failure(error.toString()));
+                    dispatch(alertActions.error(error.toString()));
+                }
+            );
+    };
+    const request=(user)=>{ return {type: userConstants.REGISTER_REQUEST, user}}
+    const success=(user)=>{ return { type: userConstants.REGISTER_SUCCESS, user } }
+    const failure=(error)=>{ return { type: userConstants.REGISTER_FAILURE, error } }
+}
